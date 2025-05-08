10b797525f0aba1ad17d3c37ab9411cbcc0f83f6
AsmaaYassin2023 <asmaadepcs@gmail.com>
2025-05-08 05:39:18 +0100

    Add classes

---
+++ Model/Comment.cs
@@ -0,0 +1,14 @@
+﻿namespace ContentInteractionService.Model
+{
+    public class Comment
+    {
+        public string Id { get; set; }
+        public string UserId { get; set; }
+        public string ImageId { get; set; }
+        public string Text { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string ImageTitle { get; set; }
+        public string ImageCaption { get; set; }
+        public string ImageLocation { get; set; }
+    }
+}
---
+++ Model/Rating.cs
@@ -0,0 +1,14 @@
+﻿namespace ContentInteractionService.Model
+{
+    public class Rating
+    {
+        public string Id { get; set; }
+        public string UserId { get; set; }
+        public string ImageId { get; set; }
+        public int RatingValue { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string ImageTitle { get; set; }
+        public string ImageCaption { get; set; }
+        public string ImageLocation { get; set; }
+    }
+}
